25307b422a9b4e502bd58acaced1ad3d752d5668
topkoong <engineer_top@msn.com>
2019-06-04 22:57:03 -0400

    Fetch HackerNews articles using useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [results, setResults] = useState([]);
+  
+	useEffect(() => {
+		getResults();
+  }, []);
+  
+	const getResults = async () => {
+		const { data } = await axios.get(
+			'http://hn.algolia.com/api/v1/search?query=reacthooks'
+		);
+		setResults(data.hits);
+	};
+	return (
+		<>
+      <input type="text"/>
+			<ul>
+				{results.map(({ objectID, url, title }) => (
+					<li key={objectID}>
+						<a href={url}>{title}</a>
+					</li>
+				))}
+			</ul>
+		</>
+	);
 }
-
-export default App;
