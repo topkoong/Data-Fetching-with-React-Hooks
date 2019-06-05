b96b8b6b6063ac74f8b1bf95ff4c62f0cdbd4e3a
topkoong <engineer_top@msn.com>
2019-06-05 00:28:09 -0400

    Displaying Loading Statw with useState

--- src/App.js
+++ src/App.js
@@ -3,53 +3,58 @@ import axios from 'axios';
 
 export default function App() {
 	const [results, setResults] = useState([]);
-  const [query, setQuery] = useState('react hooks');
-  const searchInputRef = useRef();
+	const [query, setQuery] = useState('react hooks');
+	const searchInputRef = useRef();
+	const [loading, setLoading] = useState(false);
 
 	useEffect(() => {
 		getResults();
 	}, []);
 
 	const getResults = async () => {
+    setLoading(true);
 		const { data } = await axios.get(
 			`http://hn.algolia.com/api/v1/search?query=${query}`
 		);
 		setResults(data.hits);
-  };
-  
-  const handleSearch = event => {
-    event.preventDefault();
-    getResults();
-  }
+		setLoading(false);
+	};
 
-  const handleClearSearch = () => {
-    setQuery('');
-    searchInputRef.current.focus();
-  }
+	const handleSearch = event => {
+		event.preventDefault();
+		getResults();
+	};
+
+	const handleClearSearch = () => {
+		setQuery('');
+		searchInputRef.current.focus();
+	};
 	return (
 		<>
 			<form onSubmit={handleSearch}>
 				<input
 					type="text"
 					onChange={event => setQuery(event.target.value)}
-          value={query}
-          ref={searchInputRef}
+					value={query}
+					ref={searchInputRef}
 				/>
-				<button type="submit">
-					Search
-				</button>
-        <button type="button" onClick={handleClearSearch}>
+				<button type="submit">Search</button>
+				<button type="button" onClick={handleClearSearch}>
 					Clear
 				</button>
 			</form>
 
-			<ul>
-				{results.map(({ objectID, url, title }) => (
-					<li key={objectID}>
-						<a href={url}>{title}</a>
-					</li>
-				))}
-			</ul>
+			{loading ? (
+				<div>Loading results...</div>
+			) : (
+				<ul>
+					{results.map(({ objectID, url, title }) => (
+						<li key={objectID}>
+							<a href={url}>{title}</a>
+						</li>
+					))}
+				</ul>
+			)}
 		</>
 	);
 }
