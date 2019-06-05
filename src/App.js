62040d584fe07102d6e9a1a6c1f35189bded14ef
topkoong <engineer_top@msn.com>
2019-06-05 00:52:14 -0400

    Styling our Project with TailwindCSS

--- src/App.js
+++ src/App.js
@@ -35,32 +35,35 @@ export default function App() {
 		searchInputRef.current.focus();
 	};
 	return (
-		<>
-			<form onSubmit={handleSearch}>
+		<div className='container max-w-md mx-auto p-4 m-2 bg-purple-100 shadow-lg rounded'>
+      <img src="https://icon.now.sh/react/c0c" alt="React Logo" className='float-right h-12'/>
+      <h1 className='text-gray-900 text-lg font-thin'>Hooks News</h1>
+			<form onSubmit={handleSearch} className='mb-2'>
 				<input
 					type="text"
 					onChange={event => setQuery(event.target.value)}
 					value={query}
-					ref={searchInputRef}
+          ref={searchInputRef}
+          className='border p-1 rounded'
 				/>
-				<button type="submit">Search</button>
-				<button type="button" onClick={handleClearSearch}>
+				<button type="submit" className='bg-orange-600 rounded m-1 p-1'>Search</button>
+				<button type="button" onClick={handleClearSearch} className='bg-teal-600 text-white rounded m-1 p-1'>
 					Clear
 				</button>
 			</form>
 
 			{loading ? (
-				<div>Loading results...</div>
+				<div className='font-bold text-orange-900'>Loading results...</div>
 			) : (
-				<ul>
+				<ul className='list-none leading-normal'>
 					{results.map(({ objectID, url, title }) => (
 						<li key={objectID}>
-							<a href={url}>{title}</a>
+							<a href={url} className='text-indigo-600 hover:text-indigo-900'>{title}</a>
 						</li>
 					))}
 				</ul>
       )}
-      {error && <div>{error.message}</div>}
-		</>
+      {error && <div className='text-red-600 font-bold'>{error.message}</div>}
+		</div>
 	);
 }
