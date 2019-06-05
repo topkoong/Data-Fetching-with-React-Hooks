5063b3667d34d755a6306a0f29b4caebeff8881e
topkoong <engineer_top@msn.com>
2019-06-04 23:13:00 -0400

    Using useRef Hook on our Search Input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 export default function App() {
 	const [results, setResults] = useState([]);
-	const [query, setQuery] = useState('react hooks');
+  const [query, setQuery] = useState('react hooks');
+  const searchInputRef = useRef();
 
 	useEffect(() => {
 		getResults();
@@ -20,16 +21,25 @@ export default function App() {
     event.preventDefault();
     getResults();
   }
+
+  const handleClearSearch = () => {
+    setQuery('');
+    searchInputRef.current.focus();
+  }
 	return (
 		<>
 			<form onSubmit={handleSearch}>
 				<input
 					type="text"
 					onChange={event => setQuery(event.target.value)}
-					value={query}
+          value={query}
+          ref={searchInputRef}
 				/>
 				<button type="submit">
 					Search
+				</button>
+        <button type="button" onClick={handleClearSearch}>
+					Clear
 				</button>
 			</form>
 
