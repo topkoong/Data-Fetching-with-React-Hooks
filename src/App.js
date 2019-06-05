965190330988bfff04484ea7796cef4cdbd6b94d
topkoong <engineer_top@msn.com>
2019-06-04 23:07:20 -0400

    Fetching Data upon Submitting Form

--- src/App.js
+++ src/App.js
@@ -2,22 +2,37 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function App() {
-  const [results, setResults] = useState([]);
-  const [query, setQuery] = useState('reacthooks');
-  
+	const [results, setResults] = useState([]);
+	const [query, setQuery] = useState('react hooks');
+
 	useEffect(() => {
 		getResults();
-  }, [query]);
-  
+	}, []);
+
 	const getResults = async () => {
 		const { data } = await axios.get(
 			`http://hn.algolia.com/api/v1/search?query=${query}`
 		);
 		setResults(data.hits);
-	};
+  };
+  
+  const handleSearch = event => {
+    event.preventDefault();
+    getResults();
+  }
 	return (
 		<>
-      <input type="text" onChange={event => setQuery(event.target.value)}/>
+			<form onSubmit={handleSearch}>
+				<input
+					type="text"
+					onChange={event => setQuery(event.target.value)}
+					value={query}
+				/>
+				<button type="submit">
+					Search
+				</button>
+			</form>
+
 			<ul>
 				{results.map(({ objectID, url, title }) => (
 					<li key={objectID}>
