b70faa0739f5cf6c9e3aee008a32284e6ec5e240
topkoong <engineer_top@msn.com>
2019-06-04 23:01:40 -0400

    Fetching search results on Component Update with useEffect

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import axios from 'axios';
 
 export default function App() {
   const [results, setResults] = useState([]);
+  const [query, setQuery] = useState('reacthooks');
   
 	useEffect(() => {
 		getResults();
-  }, []);
+  }, [query]);
   
 	const getResults = async () => {
 		const { data } = await axios.get(
-			'http://hn.algolia.com/api/v1/search?query=reacthooks'
+			`http://hn.algolia.com/api/v1/search?query=${query}`
 		);
 		setResults(data.hits);
 	};
 	return (
 		<>
-      <input type="text"/>
+      <input type="text" onChange={event => setQuery(event.target.value)}/>
 			<ul>
 				{results.map(({ objectID, url, title }) => (
 					<li key={objectID}>
